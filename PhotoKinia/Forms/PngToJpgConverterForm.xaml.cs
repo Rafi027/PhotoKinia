d0522439803d7c664658e99df546127ddd5ab43f
Rafał Świrk <rafalswirk@outlook.com>
2018-05-08 09:17:15 +0200

    Added png to jpg converter

--- PhotoKinia/Forms/PngToJpgConverterForm.xaml.cs
+++ PhotoKinia/Forms/PngToJpgConverterForm.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace PhotoKinia.Forms
     /// <summary>
     /// Interaction logic for PngToJpgConverterForm.xaml
     /// </summary>
-    public partial class PngToJpgConverterForm : Window
+    public partial class PngToJpgConverterForm : MetroWindow
     {
         public PngToJpgConverterForm()
         {
