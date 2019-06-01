b3f66b0e77f10907f749f2d6e2223d245da86453
Rafi <rafalswirk@outlook.com>
2019-06-01 16:20:16 +0200

    - Added sorter empty classes

---
+++ PhotoKinia/Modules/ImageSortingModule/ImageSorter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoKinia.Modules.ImageSortingModule
+{
+    public class ImageSorter
+    {
+        public bool Sort(string outputDirectory, params string[] inputDirectories)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PhotoKiniaTests/Modules/ImageSortingModule/ImageSorterTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhotoKinia.Modules.ImageSortingModule;
+
+namespace PhotoKiniaTests.Modules.ImageSortingModule
+{
+    [TestClass]
+    public class ImageSorterTestFixture
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var outputDirectory = @"C:\Photos";
+            var inputDirectories = new string[] { @"D:\Directory1", @"D:\Directory2" };
+            var imageSorter = new ImageSorter();
+            var result = imageSorter.Sort(outputDirectory, inputDirectories);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
