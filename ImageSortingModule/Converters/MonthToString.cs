a41488d06fd3fdb4f103bfc1a704135e4524af09
Rafał Świrk <rafalswirk@outlook.com>
2020-05-17 13:19:43 +0200

    Added stub class for mont conversion

---
+++ ImageSortingModule/Converters/MonthToString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageSortingModule.Converters
+{
+    class MonthToString
+    {
+        internal string Convert(int month)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
