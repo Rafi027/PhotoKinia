40356db4cd0e163c94dcf08d3e73fea6048fb171
Rafał Świrk <rafalswirk@outlook.com>
2020-05-17 12:46:18 +0200

    - Added console project for image sorting

---
+++ ImageSorting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
