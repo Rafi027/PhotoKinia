cee48dcfe43a92fd0c0426df881d8b2bbe154ec8
Rafał Świrk <rafalswirk@outlook.com>
2020-05-26 22:00:56 +0200

    - Added unit test for rename feature

---
+++ ImageSortingModule/Classification/RenameMethod/IncrementalRename.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ImageSortingModule.Classification.RenameMethod
+{
+    internal class IncrementalRename
+    {
+        public string GetNewFileName(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+            var noExtensionName = Path.GetFileNameWithoutExtension(fileName);
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PhotoKiniaTests/Modules/ImageSortingModule/RenameMethodTestFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ImageSortingModule.Classification.RenameMethod;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PhotoKiniaTests.Modules.ImageSortingModule
+{
+    [TestClass]
+    public class RenameMethodTestFixture
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var incrementalRename = new IncrementalRename();
+            var incrementedName = incrementalRename.GetNewFileName("Fooo_1.jpg");
+            Assert.AreEqual("Fooo_2.jpg", incrementalRename);
+            
+            incrementedName = incrementalRename.GetNewFileName("Fooo_2.jpg");
+            Assert.AreEqual("Fooo_3.jpg", incrementalRename);
+
+            incrementedName = incrementalRename.GetNewFileName("Fooo_22.jpg");
+            Assert.AreEqual("Fooo_23.jpg", incrementalRename);
+
+            incrementedName = incrementalRename.GetNewFileName("Fooo_a.jpg");
+            Assert.AreEqual("Fooo_a_1.jpg", incrementalRename);
+
+            incrementedName = incrementalRename.GetNewFileName("Fooo_a_a.jpg");
+            Assert.AreEqual("Fooo_a_a_1.jpg", incrementalRename);
+
+            incrementedName = incrementalRename.GetNewFileName("Fooo_1_a.jpg");
+            Assert.AreEqual("Fooo_1_a_1.jpg", incrementalRename);
+
+            incrementedName = incrementalRename.GetNewFileName("Fooo_a1_.jpg");
+            Assert.AreEqual("Fooo_a_1__1.jpg", incrementalRename);
+
+            incrementedName = incrementalRename.GetNewFileName("Fooo_a1.jpg");
+            Assert.AreEqual("Fooo_a1_1.jpg", incrementalRename);
+        }
+    }
+}
