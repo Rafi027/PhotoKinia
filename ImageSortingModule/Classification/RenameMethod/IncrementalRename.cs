26a956cf286a905d50fa800f98ddf4a7425ac4a5
Rafał Świrk <rafalswirk@outlook.com>
2020-05-28 07:53:48 +0200

    Added implementation algorithm for incremental file rename

--- ImageSortingModule/Classification/RenameMethod/IncrementalRename.cs
+++ ImageSortingModule/Classification/RenameMethod/IncrementalRename.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ImageSortingModule.Classification.RenameMethod
 {
@@ -9,10 +10,30 @@ namespace ImageSortingModule.Classification.RenameMethod
     {
         public string GetNewFileName(string fileName)
         {
+            var regex = new Regex(@"\(\d+\)$");
             var extension = Path.GetExtension(fileName);
             var noExtensionName = Path.GetFileNameWithoutExtension(fileName);
+            var matchResult = regex.Match(noExtensionName);
+            if (matchResult.Success)
+                return IncrementedName(noExtensionName, extension);
+            return noExtensionName + "(1)" + extension;
+        }
 
-            throw new NotImplementedException();
+        private string IncrementedName(string noExtensionName, string extension)
+        {
+            var regex = new Regex(@"\(\d+\)$");
+            var splittedName = regex.Split(noExtensionName);
+            var match = regex.Match(noExtensionName);
+            int id = GetID(match.Value);
+            return $"{noExtensionName.Remove(match.Index, noExtensionName.Length - match.Index)}({++id}){extension}";
+        }
+
+        private int GetID(string value)
+        {
+            var textNumber = value.Replace("(", "").Replace(")", "");
+            if (int.TryParse(textNumber, out int id))
+                return id;
+            throw new InvalidOperationException();
         }
     }
 }
