4047ae2270037d3fc29e4da38834360eb5011c5c
Rafał Świrk <rafalswirk@outlook.com>
2021-02-05 15:17:47 +0100

    - Added more tests for nested directories reduction

--- PhotoKiniaTests/Modules/FileListGeneratorModule/PathReductionTestFixture.cs
+++ PhotoKiniaTests/Modules/FileListGeneratorModule/PathReductionTestFixture.cs
@@ -23,5 +23,51 @@ namespace PhotoKiniaTests.Modules.ImageSortingModule
             Assert.AreEqual(1, output.Count);
             Assert.AreEqual(@"C:\images\m01\", output[0]);
         }
+
+        [TestMethod]
+        public void CheckReduceFromDifferentBasePaths()
+        {
+            var input = new List<string>
+            {
+                @"C:\images\m02\d01",
+                @"C:\images\m01\"
+            };
+
+            var reduction = new PathReductor();
+            List<string> output = reduction.Reduce(input);
+            Assert.IsNotNull(output);
+            Assert.AreEqual(2, output.Count);
+        }
+
+        [TestMethod]
+        public void CheckReduceForTwoBasePaths()
+        {
+            var input = new List<string>
+            {
+                @"C:\A\B\C",
+                @"C:\A",
+                @"C:\images\m01\d01",
+                @"C:\images\m02\d01",
+                @"C:\images\m01\"
+            };
+
+            var reduction = new PathReductor();
+            var output = reduction.Reduce(input);
+            Assert.AreEqual(3, output.Count);
+        }
+
+        [TestMethod]
+        public void CheckReduceForDuplication()
+        {
+            var input = new List<string>
+            {
+                @"C:\A",
+                @"C:\A",
+            };
+
+            var reduction = new PathReductor();
+            var output = reduction.Reduce(input);
+            Assert.AreEqual(1, output.Count);
+        }
     }
 }
