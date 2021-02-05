6c6973d1d27f34737b956a95432eb1af0be23de6
Rafał Świrk <rafalswirk@outlook.com>
2021-02-05 13:37:51 +0100

    - Added basic unit test for reducing nested paths

---
+++ ImageSortingModule/FileListGeneration/PathReductor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageSortingModule.FileListGeneration
+{
+    internal class PathReductor
+    {
+        internal List<string> Reduce(List<string> directories)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PhotoKiniaTests/Modules/FileListGeneratorModule/PathReductionTestFixture.cs
@@ -0,0 +1,27 @@
+﻿using ImageSortingModule.FileListGeneration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace PhotoKiniaTests.Modules.ImageSortingModule
+{
+    [TestClass]
+    public class PathReductionTestFixture
+    {
+        [TestMethod]
+        public void ReduceNestedDirectory()
+        {
+            var input = new List<string>
+            {
+                @"C:\images\m01\d01",
+                @"C:\images\m01\"
+            };
+
+            var reduction = new PathReductor();
+            List<string> output = reduction.Reduce(input);
+            Assert.IsNotNull(output);
+            Assert.AreEqual(1, output.Count);
+            Assert.AreEqual(@"C:\images\m01\", output[0]);
+        }
+    }
+}
