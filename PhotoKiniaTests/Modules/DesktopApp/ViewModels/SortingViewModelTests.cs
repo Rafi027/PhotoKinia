0fa4fc0d7ba0e24fdb609797b0f24724055bd6ca
Rafał Świrk <rafalswirk@outlook.com>
2021-05-06 18:54:51 +0100

    - Implementing basic unit test for SortingViewModel

---
+++ PhotoKiniaTests/Modules/DesktopApp/ViewModels/SortingViewModelTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PhotoKinia.Contracts;
+using PhotoKinia.ViewModels;
+using System;
+
+namespace PhotoKiniaTests.Modules.DesktopApp.ViewModels
+{
+    [TestClass]
+    public class SortingViewModelTests
+    {
+        private const string TestPath1 = @"C:\images1";
+        private const string TestPath2 = @"C:\images2";
+        private const string TestPath3 = @"C:\images3";
+
+        [TestMethod]
+        public void TestAddDirectoryMethod()
+        {
+            var directorySelectorMock = new Mock<IDirectorySelector>();
+            directorySelectorMock.SetupSequence(d => d.SelectDirectory())
+                .Returns(TestPath1)
+                .Returns(TestPath2)
+                .Returns(TestPath3);
+
+            var ViewModel = new SortingViewModel(directorySelectorMock.Object);
+            ViewModel.AddDirectory.Execute(null);
+            ViewModel.AddDirectory.Execute(null);
+            ViewModel.AddDirectory.Execute(null);
+
+            Assert.AreEqual(3, ViewModel.InputDirectories.Count);
+            Assert.AreEqual(TestPath1, ViewModel.InputDirectories[0]);
+            Assert.AreEqual(TestPath2, ViewModel.InputDirectories[1]);
+            Assert.AreEqual(TestPath3, ViewModel.InputDirectories[2]);
+        }
+    }
+}
