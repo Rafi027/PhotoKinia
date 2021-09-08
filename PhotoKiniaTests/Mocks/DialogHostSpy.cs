a865dad9a15900cdd46d7208ba8ecfb7b65a1630
Rafał Świrk <rafalswirk@outlook.com>
2021-09-08 13:24:40 +0200

    - Fixed unit tests for copy mode selection

--- PhotoKiniaTests/Mocks/DialogHostSpy.cs
+++ PhotoKiniaTests/Mocks/DialogHostSpy.cs
@@ -1,9 +1,5 @@
 ﻿using MaterialDesignThemes.Wpf;
 using PhotoKinia.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace PhotoKiniaTests.Mocks
@@ -19,6 +15,8 @@ namespace PhotoKiniaTests.Mocks
             PassedObject = o;
             DialogID = dialogID;
             NumberOfCalls++;
+
+            openedEventHandler.Invoke(this, null);
             return Task.CompletedTask;
         }
     }
