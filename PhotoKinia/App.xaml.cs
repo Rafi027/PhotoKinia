639c4e1688eb4db8378fcd2c8ed3e4bd66efb7b0
Rafał Świrk <rafalswirk@outlook.com>
2021-09-09 11:14:51 +0200

    - Selecting app language using input arguments

--- PhotoKinia/App.xaml.cs
+++ PhotoKinia/App.xaml.cs
@@ -14,9 +14,17 @@ namespace PhotoKinia
     /// </summary>
     public partial class App : Application
     {
-        public App()
+        private void Application_Startup(object sender, StartupEventArgs e)
         {
+            if (e.Args.Length == 0)
+                return;
+
+            var simplifiedArgs = string.Join(" ", e.Args);
             var language = "en-EN";
+            if (simplifiedArgs.Equals("--language -en"))
+                language = "en-EN";
+            if (simplifiedArgs.Equals("--language -pl"))
+                language = "en-EN";
             CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(language);
             CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(language);
         }
