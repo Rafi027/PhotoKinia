b81579630ed59aa9956f149317532127bef5dc5f
Rafał Świrk <rafalswirk@outlook.com>
2021-09-09 10:43:31 +0200

    - Adding missing text resources to UI

--- PhotoKinia/App.xaml.cs
+++ PhotoKinia/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,11 @@ namespace PhotoKinia
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            var language = "en-EN";
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(language);
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(language);
+        }
     }
 }
